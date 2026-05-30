a047fabd583e4ffdcd409dff12ee1f941a55da11
helisoya <oudot.julien@laposte.net>
2026-05-30 12:55:37 +0200

    Screen Settings

--- Assets/Scripts/ANF/Persistent/Containers/ScreenSettingsContainer.cs
+++ Assets/Scripts/ANF/Persistent/Containers/ScreenSettingsContainer.cs
@@ -1,6 +1,8 @@
 
 using Leguar.TotalJSON;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace ANF.Persistent
 {
@@ -10,13 +12,19 @@ namespace ANF.Persistent
     [System.Serializable]
     public class ScreenSettingsContainer : DataContainer
     {
-        [Tooltip("If true, Screen.currentResolution will be used as a default")]
+        [Tooltip("If true, current Screen/QualitySettings data will be used as a default for ALL parameters")]
         [SerializeField] private bool useScreenSettings = true;
         [SerializeField] private bool defaultFullscreen = true;
         [SerializeField] private int defaultWidth = 800;
         [SerializeField] private int defaultHeight = 600;
+        [SerializeField] private int defaultVSyncCount = 1;
+        [SerializeField] private int defaultAntiAliasing = 2;
+        [SerializeField] private int defaultShadowCascadeCount = 4;
         private Resolution resolution;
         private bool fullscreen;
+        private int vSyncCount;
+        private int antiAliasing;
+        private int shadowCascadeCount;
 
         public DataContainer CloneContainer()
         {
@@ -25,7 +33,10 @@ namespace ANF.Persistent
                 defaultFullscreen = defaultFullscreen,
                 useScreenSettings = useScreenSettings,
                 defaultWidth = defaultWidth,
-                defaultHeight = defaultHeight
+                defaultHeight = defaultHeight,
+                defaultVSyncCount = defaultVSyncCount,
+                defaultAntiAliasing = defaultAntiAliasing,
+                defaultShadowCascadeCount = defaultShadowCascadeCount
             };
         }
 
@@ -40,6 +51,11 @@ namespace ANF.Persistent
             {
                 resolution = Screen.currentResolution;
                 fullscreen = Screen.fullScreen;
+                vSyncCount = QualitySettings.vSyncCount = vSyncCount;
+
+                UniversalRenderPipelineAsset pipelineAsset = GraphicsSettings.currentRenderPipeline as UniversalRenderPipelineAsset;
+                antiAliasing = pipelineAsset.msaaSampleCount;
+                shadowCascadeCount = pipelineAsset.shadowCascadeCount;
             }
             else
             {
@@ -47,6 +63,9 @@ namespace ANF.Persistent
 
                 SetFullscreen(defaultFullscreen);
                 SetResolution(resolution);
+                SetVSyncCount(defaultVSyncCount);
+                SetAntiAliasing(antiAliasing);
+                SetShadowQuality(shadowCascadeCount);
             }
         }
 
@@ -59,6 +78,15 @@ namespace ANF.Persistent
 
             if (json.ContainsKey("fullscreen"))
                 fullscreen = json.GetBool("fullscreen");
+
+            if (json.ContainsKey("vSyncCount"))
+                vSyncCount = json.GetInt("vSyncCount");
+
+            if (json.ContainsKey("antiAliasing"))
+                antiAliasing = json.GetInt("antiAliasing");
+
+            if (json.ContainsKey("shadows"))
+                shadowCascadeCount = json.GetInt("shadows");
 
             if (json.ContainsKey("width"))
                 width = json.GetInt("width");
@@ -76,6 +104,9 @@ namespace ANF.Persistent
                 refreshRateRatio = new() { denominator = denominator, numerator = numerator }
             });
             SetFullscreen(fullscreen);
+            SetVSyncCount(vSyncCount);
+            SetAntiAliasing(antiAliasing);
+            SetShadowQuality(shadowCascadeCount);
         }
 
         public void Save(JSON json)
@@ -85,6 +116,9 @@ namespace ANF.Persistent
             json.Add("height", resolution.height);
             json.Add("refreshRateDenomiator", resolution.refreshRateRatio.denominator);
             json.Add("refreshRateNumerator", resolution.refreshRateRatio.numerator);
+            json.Add("vSyncCount", vSyncCount);
+            json.Add("antiAliasing", antiAliasing);
+            json.Add("shadows", shadowCascadeCount);
         }
 
         /// <summary>
@@ -110,6 +144,38 @@ namespace ANF.Persistent
         }
 
         /// <summary>
+		/// Sets the game's vSyncCount (0 to disable, 1 to enable)
+		/// </summary>
+		/// <param name="vSyncCount">The VSync Count</param>
+        public void SetVSyncCount(int vSyncCount)
+        {
+            this.vSyncCount = vSyncCount;
+            QualitySettings.vSyncCount = vSyncCount;
+        }
+
+        /// <summary>
+		/// Sets the game's antialiasing (0, 2 4 and 8)
+		/// </summary>
+		/// <param name="antiAliasing">The antialiasing value</param>
+        public void SetAntiAliasing(int antiAliasing)
+        {
+            this.antiAliasing = antiAliasing;
+            UniversalRenderPipelineAsset pipelineAsset = GraphicsSettings.currentRenderPipeline as UniversalRenderPipelineAsset;
+            pipelineAsset.msaaSampleCount = antiAliasing;
+        }
+
+        /// <summary>
+		/// Sets the game's shadow quality
+		/// </summary>
+		/// <param name="cascadeCount">The shadow's cascade count</param>
+        public void SetShadowQuality(int cascadeCount)
+        {
+            this.shadowCascadeCount = cascadeCount;
+            UniversalRenderPipelineAsset pipelineAsset = GraphicsSettings.currentRenderPipeline as UniversalRenderPipelineAsset;
+            pipelineAsset.shadowCascadeCount = cascadeCount;
+        }
+
+        /// <summary>
         /// Gets if the game is in fullscreen or not
         /// </summary>
         /// <returns>True if in fullscreen</returns>
@@ -119,13 +185,39 @@ namespace ANF.Persistent
         }
 
         /// <summary>
-        /// Gets the current gam's resolution
+        /// Gets the current game's resolution
         /// </summary>
         /// <returns>The current resolution</returns>
         public Resolution GetResolution()
         {
             return resolution;
         }
-    }
 
+        /// <summary>
+		/// Gets the current vsync count
+		/// </summary>
+		/// <returns>The Vsync count</returns>
+        public int GetVSyncCount()
+        {
+            return vSyncCount;
+        }
+
+        /// <summary>
+		/// Gets the game's antialiasing value
+		/// </summary>
+		/// <returns>The antialiasing value</returns>
+        public int GetAntiAliasing()
+        {
+            return antiAliasing;
+        }
+
+        /// <summary>
+		/// Gets the shadow's quality
+		/// </summary>
+		/// <returns>The shadow's quality</returns>
+        public int GetShadowQuality()
+        {
+            return shadowCascadeCount;
+        }
+    }
 }
--- Assets/Scripts/ANF/Scene/GUI/SettingsMenu/Tabs/SettingsTabUIGraphics.cs
+++ Assets/Scripts/ANF/Scene/GUI/SettingsMenu/Tabs/SettingsTabUIGraphics.cs
@@ -14,6 +14,9 @@ namespace ANF.GUI
     {
         private Toggle fullscreenToggle;
         private TMP_Dropdown resolutionDropdown;
+        private TMP_Dropdown antiAliasingDropdown;
+        private TMP_Dropdown shadowsDropdown;
+        private Toggle vSyncToggle;
 
         public override string GetLabelKey()
         {
@@ -24,48 +27,117 @@ namespace ANF.GUI
         {
             if (PersistentDataManager.instance.GetGlobalData().GetComponent(out ScreenSettingsContainer screenSettings))
             {
-                fullscreenToggle = menu.CreateToggle("SettingsMenu_Graphics_Fullscreen", root);
-                fullscreenToggle.SetIsOnWithoutNotify(screenSettings.IsFullscreen());
-                fullscreenToggle.onValueChanged.AddListener((bool fullscreen) =>
-                {
-                    screenSettings.SetFullscreen(fullscreen);
-                });
 
-                Resolution[] resolutions = Screen.resolutions;
-                Resolution currentResolution = screenSettings.GetResolution();
-                List<string> resolutionsLabels = new List<string>();
-                int currentIdx = -1;
-
-                for (int i = 0; i < resolutions.Length; i++)
                 {
-                    resolutionsLabels.Add($"{resolutions[i].width}/{resolutions[i].height}({Math.Round(resolutions[i].refreshRateRatio.value, 2)})");
-                    if (currentIdx == -1 && resolutions[i].width == currentResolution.width &&
-                        resolutions[i].height == currentResolution.height && resolutions[i].refreshRateRatio.value == currentResolution.refreshRateRatio.value)
+                    fullscreenToggle = menu.CreateToggle("SettingsMenu_Graphics_Fullscreen", root);
+                    fullscreenToggle.SetIsOnWithoutNotify(screenSettings.IsFullscreen());
+                    fullscreenToggle.onValueChanged.AddListener((bool fullscreen) =>
                     {
-                        currentIdx = i;
-                    }
+                        screenSettings.SetFullscreen(fullscreen);
+                    });
                 }
 
-                if (currentIdx == -1)
-                    currentIdx = 0;
-
-                resolutionDropdown = menu.CreateDropdown("SettingsMenu_Graphics_Resolution", root);
-                resolutionDropdown.ClearOptions();
-                resolutionDropdown.AddOptions(resolutionsLabels);
-                resolutionDropdown.SetValueWithoutNotify(currentIdx);
-                resolutionDropdown.onValueChanged.AddListener((int idx) =>
                 {
-                    screenSettings.SetResolution(resolutions[idx]);
-                });
+                    Resolution[] resolutions = Screen.resolutions;
+                    Resolution currentResolution = screenSettings.GetResolution();
+                    List<string> resolutionsLabels = new List<string>();
+                    int currentIdx = -1;
+
+                    for (int i = 0; i < resolutions.Length; i++)
+                    {
+                        resolutionsLabels.Add($"{resolutions[i].width}/{resolutions[i].height}({Math.Round(resolutions[i].refreshRateRatio.value, 2)})");
+                        if (currentIdx == -1 && resolutions[i].width == currentResolution.width &&
+                            resolutions[i].height == currentResolution.height && resolutions[i].refreshRateRatio.value == currentResolution.refreshRateRatio.value)
+                        {
+                            currentIdx = i;
+                        }
+                    }
+
+                    if (currentIdx == -1)
+                        currentIdx = 0;
+
+                    resolutionDropdown = menu.CreateDropdown("SettingsMenu_Graphics_Resolution", root);
+                    resolutionDropdown.ClearOptions();
+                    resolutionDropdown.AddOptions(resolutionsLabels);
+                    resolutionDropdown.SetValueWithoutNotify(currentIdx);
+                    resolutionDropdown.onValueChanged.AddListener((int idx) =>
+                    {
+                        screenSettings.SetResolution(resolutions[idx]);
+                    });
+                }
+
+                {
+                    vSyncToggle = menu.CreateToggle("SettingsMenu_Graphics_VSync", root);
+                    vSyncToggle.SetIsOnWithoutNotify(screenSettings.GetVSyncCount() != 0);
+                    vSyncToggle.onValueChanged.AddListener((bool vSync) =>
+                    {
+                        screenSettings.SetVSyncCount(vSync ? 1 : 0);
+                    });
+                }
+
+                {
+                    int[] antiAliasingValues = { 0, 2, 4, 8 };
+                    List<string> antiAliasingValuesLabels = new List<string>(
+                        new string[] { "x0", "x2", "x4", "x8" }
+                        );
+                    int selectedIndex = Array.IndexOf(antiAliasingValues, screenSettings.GetAntiAliasing());
+                    if (selectedIndex == -1)
+                        selectedIndex = 0;
+
+                    antiAliasingDropdown = menu.CreateDropdown("SettingsMenu_Graphics_AntiAliasing", root);
+                    antiAliasingDropdown.ClearOptions();
+                    antiAliasingDropdown.AddOptions(antiAliasingValuesLabels);
+                    antiAliasingDropdown.SetValueWithoutNotify(selectedIndex);
+                    antiAliasingDropdown.onValueChanged.AddListener((int idx) =>
+                    {
+                        screenSettings.SetAntiAliasing(antiAliasingValues[idx]);
+                    });
+                }
+
+                {
+                    PersistentDataManager.instance.GetGlobalData().GetComponent(out Locals.Locals locals);
+
+                    List<string> labels = new List<string>(
+                        new string[]
+                        {
+                            locals != null ? locals.GetLocal("SettingsMenu_Graphics_Shadows_1") : "SettingsMenu_Graphics_Shadows_1",
+                            locals != null ? locals.GetLocal("SettingsMenu_Graphics_Shadows_2") : "SettingsMenu_Graphics_Shadows_2",
+                            locals != null ? locals.GetLocal("SettingsMenu_Graphics_Shadows_3") : "SettingsMenu_Graphics_Shadows_3",
+                            locals != null ? locals.GetLocal("SettingsMenu_Graphics_Shadows_4") : "SettingsMenu_Graphics_Shadows_4"
+                        }
+                        );
+
+                    shadowsDropdown = menu.CreateDropdown("SettingsMenu_Graphics_Shadows", root);
+                    shadowsDropdown.ClearOptions();
+                    shadowsDropdown.AddOptions(labels);
+                    shadowsDropdown.SetValueWithoutNotify(screenSettings.GetShadowQuality() - 1);
+                    shadowsDropdown.onValueChanged.AddListener((int idx) =>
+                    {
+                        screenSettings.SetShadowQuality(idx + 1);
+                    });
+                }
             }
-
-
         }
 
         public override void RedrawLocalizedElements()
         {
+            if (!PersistentDataManager.instance.GetGlobalData().GetComponent(out ScreenSettingsContainer screenSettings))
+                return;
 
+            PersistentDataManager.instance.GetGlobalData().GetComponent(out Locals.Locals locals);
+
+            List<string> labels = new List<string>(
+                new string[]
+                {
+                    locals != null ? locals.GetLocal("SettingsMenu_Graphics_Shadows_1") : "SettingsMenu_Graphics_Shadows_1",
+                    locals != null ? locals.GetLocal("SettingsMenu_Graphics_Shadows_2") : "SettingsMenu_Graphics_Shadows_2",
+                    locals != null ? locals.GetLocal("SettingsMenu_Graphics_Shadows_3") : "SettingsMenu_Graphics_Shadows_3",
+                    locals != null ? locals.GetLocal("SettingsMenu_Graphics_Shadows_4") : "SettingsMenu_Graphics_Shadows_4"
+                });
+
+            shadowsDropdown.ClearOptions();
+            shadowsDropdown.AddOptions(labels);
+            shadowsDropdown.SetValueWithoutNotify(screenSettings.GetShadowQuality());
         }
     }
-
 }
